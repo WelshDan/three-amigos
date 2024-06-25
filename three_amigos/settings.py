5527c9c04979ae548dab1a58c7256379cce0d09e
WelshDan <dan.roberts78@gmail.com>
2024-06-25 08:16:27 +0000

    Create superuser and test it, update settings

--- three_amigos/settings.py
+++ three_amigos/settings.py
@@ -11,22 +11,28 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--%2kpnkhb^@p+2r_ye1o65op9hmo4yi&_bnj7zbawn$yzx!o3+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = ['8000-welshdan-threeamigos-e9f628v6fh7.ws-eu114.gitpod.io',
-                '.herokuapp.com']
+ALLOWED_HOSTS = [
+                '8000-welshdan-threeamigos-osmgptvz1ej.ws-eu114.gitpod.io',
+                '.herokuapp.com'
+                ]
 
 
 # Application definition
@@ -50,6 +56,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+SITE_ID = 1
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ROOT_URLCONF = 'three_amigos.urls'
 
@@ -123,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-welshdan-threeamigos-osmgptvz1ej.ws-eu114.gitpod.io',
+]
