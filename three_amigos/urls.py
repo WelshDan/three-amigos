a4f0f834dd28d09689c882e56ccc2cf6d16d1c29
WelshDan <dan.roberts78@gmail.com>
2024-11-14 12:10:17 +0000

    Ran collectstatic

--- three_amigos/urls.py
+++ three_amigos/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from bookings import views
@@ -29,6 +31,6 @@ urlpatterns = [
     path('bookings/', include('bookings.urls', namespace="bookings")),
     path('enquiry/', include('enquiries.urls', namespace="enquiry")),
     path('dates/', include('dates.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'bookings.views.error_404'
