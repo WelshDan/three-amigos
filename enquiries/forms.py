bcca561bcaf594e34eb25bf02523bc04c1877266
WelshDan <dan.roberts78@gmail.com>
2024-11-17 20:08:02 +0000

    Edit datetime field widgets

--- enquiries/forms.py
+++ enquiries/forms.py
@@ -5,7 +5,10 @@ from .models import Enquiry
 class EnquiryForm(forms.ModelForm):
 
     enquiry_when = forms.DateTimeField(
-    widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+    widget=forms.DateTimeInput(
+        format='%d-%m-%Y %H:%M',
+        attrs={'type': 'datetime-local'}
+        ),
     label="Preferred Date and Time"
     )
 
