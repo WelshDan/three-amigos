5e1627348fb243f83db0d3f41ae49c082cafbb1b
WelshDan <dan.roberts78@gmail.com>
2024-12-11 22:51:40 +0000

    Created tab windows for info page

--- temp-delete-when-completed.py
+++ temp-delete-when-completed.py
@@ -22,6 +22,126 @@ vmit8bqsm403wpk770oic8pjx691iqtnto847iii
 API url
 https://sheetdb.io/api/v1/fvi6b7rkv2z2b
 
-Temporary removal:
+Temporary:
 
+<section id="questions" class="questions section-bg-b">
+        <div class="container">
+
+            <div class="section-title">
+                <h2>Questions</h2>
+                <p>Three Amigos</p>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <ul class="nav nav-tabs flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active show" data-bs-toggle="tab" href="#tab-1">The Three Amigos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" href="#tab-2">How long have we hosted at Wirströms Pub?</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" href="#tab-3">How do we attend a quiz at Wirströms Pub?</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" href="#tab-4">Can we buy a quiz?</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" href="#tab-5">Traditional Swedish</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" href="#tab-6">Side dishes & extras</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-9 mt-4 mt-lg-0">
+                    <div class="tab-content">
+                        <div class="tab-pane active show" id="tab-1">
+                            <div class="row">
+                                <div class="col-lg-8 details order-2 order-lg-1">
+                                    <h3>Who are The Three Amigos</h3>
+                                    <p>The quiz started with three quizmasters (hence the name) but we have been two for the past few years. We have had a few guest quizmasters over the years too.
+                                    </p>
+                                </div>
+                                <div class="col-lg-4 text-center order-1 order-lg-2">
+                                    <img src="{% static 'media/menu-section/hamburger.jpg' %}" alt="" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane" id="tab-2">
+                            <div class="row">
+                                <div class="col-lg-8 details order-2 order-lg-1">
+                                    <h3>How long have we hosted the quiz at Wirströms Pub?</h3>
+                                    <p>We have been hosting the quiz for almost 10 years so we have probably done close to 200 quizzes at Wirströms Pub.</p>
+                                </div>
+                                <div class="col-lg-4 text-center order-1 order-lg-2">
+                                    <img src="{% static 'media/menu-section/fish-and-chips.jpg' %}" alt=""
+                                        class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane" id="tab-3">
+                            <div class="row">
+                                <div class="col-lg-8 details order-2 order-lg-1">
+                                    <h3>How do we attend a quiz at Wirströms Pub?</h3>
+                                    <p>We host quizzes every second Thursday that start at 19.00. Tables can be booked by emailing booking@wirstromspub.se</p>
+                                    <p><a class="pub-link" href="https://wirstromspub.se#wirstroms"
+                                        target="_blank">Wirströms Pub
+                                        </a> in <a class="pub-link" href="https://maps.app.goo.gl/zw4M4fxcm5YZTcyJ7"
+                                        target="_blank">Gamla Stan</a> (Stockholm) -
+                                        See <a href="{% url 'dates' %}" class="about-us-links">dates</a> and
+                                        <a href="{% url 'results' %}" class="about-us-links">results</a> for more information.
+                                    </p>
+                                </div>
+                                <div class="col-lg-4 text-center order-1 order-lg-2">
+                                    <img src="{% static 'media/menu-section/meats.jpg' %}" alt="" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane" id="tab-4">
+                            <div class="row">
+                                <div class="col-lg-8 details order-2 order-lg-1">
+                                    <h3>Can we buy a quiz that we can host ourselves?</h3>
+                                    <p>Send us an email on threeamigosquiz@gmail.com and we can discuss </p>
+                                </div>
+                                <div class="col-lg-4 text-center order-1 order-lg-2">
+                                    <img src="{% static 'media/menu-section/healthy.jpg' %}" alt="" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane" id="tab-5">
+                            <div class="row">
+                                <div class="col-lg-8 details order-2 order-lg-1">
+                                    <h3>Choose from a few of the Swedish favourites</h3>
+                                    <p class="fst-italic">If you prefer a classic Swedish then here you go.</p>
+                                    <p>Never tried meatballs? Now is your chance!
+                                        Do you like pea soup with pancakes to follow? We've got it here.
+                                    </p>
+                                </div>
+                                <div class="col-lg-4 text-center order-1 order-lg-2">
+                                    <img src="{% static 'media/menu-section/meatballs.jpg' %}" alt="" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane" id="tab-6">
+                            <div class="row">
+                                <div class="col-lg-8 details order-2 order-lg-1">
+                                    <h3>Side dishes for smaller meals or extras</h3>
+                                    <p class="fst-italic">A good list if you want a small snack or add more to your
+                                        order</p>
+                                    <p>We have lots of potential smaller meals including soup, cheese on toast,
+                                        nachos & cheese, ors
+                                        chicken wings. Extras include a range of vegetables and sauces.</p>
+                                </div>
+                                <div class="col-lg-4 text-center order-1 order-lg-2">
+                                    <img src="{% static 'media/menu-section/soup.jpg' %}" alt="" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
 '''
